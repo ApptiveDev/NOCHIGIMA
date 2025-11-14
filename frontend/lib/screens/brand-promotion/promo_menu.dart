0818fe558404a7f12e287eaecacf0a2cb68ea88e
Yujin <0505ujin@naver.com>
2025-11-14 16:27:10 +0900

    floating 검색 버튼 ui구현

--- frontend/lib/screens/brand-promotion/promo_menu.dart
+++ frontend/lib/screens/brand-promotion/promo_menu.dart
@@ -26,7 +26,21 @@ class PromoMenu extends StatelessWidget {
         ),
       ),
       // floating search
-      floatingActionButton: FloatingActionButton(onPressed: (){})
+      floatingActionButton: Container(
+        height: 60,
+        width: 60,
+
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.search, color: Colors.grey[600], size: 35),
+          shape: CircleBorder(
+            side: BorderSide(color: Colors.grey[200]!, width: 1.0),
+          ),
+          elevation: 4.5,
+          backgroundColor: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
