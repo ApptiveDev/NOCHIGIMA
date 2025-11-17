b83b81e15cde1c6849dfc634821608313fd328f0
tae <kyung3900@gmail.com>
2025-11-17 14:35:01 +0900

    feat: 내비바 구현 (#8)
    * feat: 내비바 구현
    
    * feat: 내비바 구현
    
    * feat: 내비바 구현
    
    * feat: 내비바 구현

---
+++ frontend/lib/screens/brand-promotion/promo_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PromoScreen extends StatelessWidget{
+  const PromoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const Center(
+      child: Text('PromoScreen'),
+    );
+  }
+}
