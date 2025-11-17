e363bba439f6038f3b393303bd103434b67325fa
yujin5047 <167182662+yujin5047@users.noreply.github.com>
2025-11-17 15:27:17 +0900

    front: login ui구현 (#6)
    * settingName.dart [1]
    
    before name typing
    
    * settingName 화면 구현 완료
    
    글자수(2~8자) 활성화 O
    버튼 색 수정 O
    구현필요:
    다음페이지 넘어가기,
    백으로 닉네임 전송하기
    
    * setting_name 완료 페이지 구현
    
    * setting_name_complete.dart 화면 구성 수정
    
    * login page 구현
    
    카카오, 구글 로그인 연동
    _handleLogin 함수 수정 필요
    
    ---------
    
    Co-authored-by: Yujin <0505ujin@naver.com>

---
+++ frontend/lib/screens/login/setting_name.dart
@@ -0,0 +1,130 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:frontend/screens/login/setting_name_complete.dart';
+
+class Settingname extends StatefulWidget {
+  const Settingname({super.key});
+
+  @override
+  State<Settingname> createState() => _SettingnameState();
+}
+
+class _SettingnameState extends State<Settingname> {
+  final _nicknameController = TextEditingController();
+  bool _isButtonEnabled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _nicknameController.addListener(_validateInput);
+  }
+
+  @override
+  void dispose() {
+    _nicknameController.dispose();
+    super.dispose();
+  }
+
+  void _validateInput() {
+    setState(() {
+      int textLength = _nicknameController.text.length;
+      _isButtonEnabled = textLength >= 2 && textLength <= 8;
+    });
+  }
+
+  void submitButton(String nickname) {
+    // nickname needs to be sent to backend
+    print("제출할 닉네임: $nickname");
+    //go to next page
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => Namecomplete(nickName: nickname)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.white, elevation: 0),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "닉네임을 입력하세요",
+              style: TextStyle(
+                color: Colors.grey[800],
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              child: TextField(
+                controller: _nicknameController,
+                decoration: InputDecoration(
+                  hintText: "닉네임 입력",
+                  hintStyle: TextStyle(color: Colors.grey[400]),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFFFF333F),
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFFFF333F),
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "2~8자까지 입력할 수 있어요.",
+              style: TextStyle(
+                color: Colors.grey[400],
+                fontSize: 14,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            Spacer(),
+            Container(
+              width: double.infinity,
+              height: 60,
+              child: ElevatedButton(
+                onPressed: _isButtonEnabled
+                    ? () {
+                        submitButton(_nicknameController.text);
+                      }
+                    : null,
+                child: Text(
+                  "다음",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFFF333F),
+                  disabledBackgroundColor: Color(0xFFFFD9DC),
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
