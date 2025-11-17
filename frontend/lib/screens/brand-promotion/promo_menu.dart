7a12330608828cbedde316d3a762f97d9822b566
Yujin <0505ujin@naver.com>
2025-11-17 14:20:47 +0900

    카테고리 바 수정, 검색 창 구현

--- frontend/lib/screens/brand-promotion/promo_menu.dart
+++ frontend/lib/screens/brand-promotion/promo_menu.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/brand-promotion/search_promotion.dart';
 
-
 class PromoMenu extends StatefulWidget {
   const PromoMenu({super.key});
 
@@ -10,43 +9,82 @@ class PromoMenu extends StatefulWidget {
 }
 
 class _PromoMenuState extends State<PromoMenu> {
-  Widget _buildMenuItem(int index, String label, String image){
-    return InkWell(
+  int _selectedIndex = 0;
 
+  Widget _buildMenuItem(int index, String label, String image) {
+    bool isSelected = (_selectedIndex == index);
+    return InkWell(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(12, 0, 12, 12),
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: isSelected
+                ? BorderSide(color: Colors.grey[800]!, width: 2)
+                : BorderSide(color: Colors.grey[200]!, width: 0.5),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 13),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: isSelected ? FontWeight.w800 : FontWeight.w600,
+                color: isSelected ? Colors.black : Colors.grey[600],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Container(
-                height: 80,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    _buildMenuItem("피자", ""),
-                    _buildMenuItem("햄버거", ""),
-                    _buildMenuItem("카페", ""),
-                    _buildMenuItem("떡볶이", ""),
-                    _buildMenuItem("편의점", ""),
-                    _buildMenuItem("기타", ""),
-                  ],
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Container(
+                  height: 80,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      _buildMenuItem(0, "피자", ""),
+                      _buildMenuItem(1, "햄버거", ""),
+                      _buildMenuItem(2, "카페", ""),
+                      _buildMenuItem(3, "떡볶이", ""),
+                      _buildMenuItem(4, "편의점", ""),
+                      _buildMenuItem(5, "기타", ""),
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                //filter
-              ),
-              Container(
-                //promo view
-                // click promo -> detail page로 넘어가도록
-              ),
-            ],
+                Container(
+                  //filter
+                  height: 40,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+        
+                    ],
+                  ),
+                ),
+                Container(
+                  //promo view
+                  // click promo -> detail page로 넘어가도록
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -59,7 +97,7 @@ class _PromoMenuState extends State<PromoMenu> {
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context)=>SearchPromotion()),
+              MaterialPageRoute(builder: (context) => SearchPromotion()),
             );
           },
           child: Icon(Icons.search, color: Colors.grey[600], size: 35),
--- frontend/lib/screens/brand-promotion/search_promotion.dart
+++ frontend/lib/screens/brand-promotion/search_promotion.dart
@@ -11,8 +11,50 @@ class _SearchPromotionState extends State<SearchPromotion> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("SearchPromotion page"),
+      appBar: AppBar(
+        toolbarHeight: 80,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+        leading: IconButton(
+          icon: Icon(Icons.navigate_before_rounded),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: SearchBar(
+
+          hintText: "매장명, 프로모션 검색",
+          hintStyle: MaterialStateProperty.all(
+            TextStyle(
+              color: Colors.grey[400],
+              fontSize: 18,
+              fontWeight: FontWeight.w500
+            )
+          ),
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.grey[050]!),
+          elevation: MaterialStateProperty.all<double>(0.0),
+          trailing: [Icon(Icons.search)],
+          shape: MaterialStateProperty.all(
+            ContinuousRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          ),
+          constraints: BoxConstraints(maxHeight: ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              // text
+            ),
+            Container(
+              // 인기 순위
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
