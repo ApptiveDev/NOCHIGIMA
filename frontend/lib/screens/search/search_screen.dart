4b25ecbfbbd8c21c4684af2422af0a4a01b7d0b4
taekoong <kyung3900@gmail.com>
2025-11-24 16:48:28 +0900

    feat: 검색 화면 구현

--- frontend/lib/screens/search/search_screen.dart
+++ frontend/lib/screens/search/search_screen.dart
@@ -1,13 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/widgets/brand-promotion/popular_search_section.dart';
 
-class SearchScreen extends StatelessWidget{
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController _resetController = TextEditingController();
+
+  @override
+  void dispose(){
+    _resetController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return const Center(
-      child: Text('SearchScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Color(0xFFAFB8C1)),
+        title: SearchBar(
+          controller: _resetController,
+          hintText: "주소 검색",
+          hintStyle: MaterialStateProperty.all(
+            TextStyle(
+              color: Colors.grey[400],
+              fontFamily: "Pretendard",
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.grey[050]!),
+          elevation: MaterialStateProperty.all<double>(0.0),
+          trailing: [
+            IconButton(onPressed: () {_resetController.clear();}, icon: Icon(Icons.cancel)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          ],
+          shape: MaterialStateProperty.all(
+            ContinuousRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          ),
+          constraints: BoxConstraints(maxHeight: 60),
+        ),
+      ),
+      // 인기 검색어
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+          ],
+        ),
+      ),
     );
   }
 }
