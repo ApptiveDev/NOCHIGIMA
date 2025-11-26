b2e9af8ee3aa579e39d856f4e8c118814a472921
yujin5047 <167182662+yujin5047@users.noreply.github.com>
2025-11-26 22:42:33 +0900

    feat: menuCategory 별로 데이터 랜더링 (#23)
    * design: MenuCategory onclick 들뜸 제거, width 조절
    
    * feat : 검색어 클릭 가능하게
    
    * feat: 인기검색어 클릭 시 searchbar로
    
    * feat: promoblock클릭 -> detailPromotion으로 이동
    
    * feat: MenuCategory별 데이터 렌더링 전환
    
    ---------
    
    Co-authored-by: Yujin <0505ujin@naver.com>

---
+++ frontend/lib/models/promotion_data.dart
@@ -0,0 +1,11 @@
+class PromotionData{
+  final String imageURL;
+  final String title;
+  final String deadline;
+
+  PromotionData({
+    required this.imageURL,
+    required this.title,
+    required this.deadline,
+  });
+}
--- frontend/lib/screens/brand-promotion/promo_screen.dart
+++ frontend/lib/screens/brand-promotion/promo_screen.dart
@@ -4,14 +4,12 @@ import 'package:frontend/screens/brand-promotion/detail_promo_screen.dart';
 import 'package:frontend/screens/brand-promotion/search_promo_screen.dart';
 import 'package:frontend/widgets/brand-promotion/brand_promotion_widgets.dart';
 import 'package:frontend/models/menu_category.dart';
+import 'package:frontend/models/promotion_data.dart';
 
 class PromoScreen extends StatefulWidget {
   final MenuCategory initialCategory;
 
-  const PromoScreen({
-    super.key,
-    this.initialCategory = MenuCategory.pizza,
-  });
+  const PromoScreen({super.key, this.initialCategory = MenuCategory.pizza});
 
   @override
   State<PromoScreen> createState() => _PromoScreenState();
@@ -21,8 +19,41 @@ class PromoScreen extends StatefulWidget {
 class _PromoScreenState extends State<PromoScreen> {
   late MenuCategory _selectedCategory;
 
+  // 임시데이터
+  final Map<MenuCategory, List<PromotionData>> _dummyData = {
+    MenuCategory.pizza: [
+      PromotionData(
+        imageURL: "assets/images/test_Mask group.jpg",
+        title: "피자헛 10% 할인",
+        deadline: "2025.09.12 ~",
+      ),
+      PromotionData(
+        imageURL: "assets/images/test_Mask group.jpg",
+        title: "도미노 1+1",
+        deadline: "2025.10.11 ~ 2025.10.18",
+      ),
+    ],
+    MenuCategory.burger: [
+      PromotionData(
+        imageURL: "assets/images/logo_burgerking.png",
+        title: "버거킹 와퍼 행사",
+        deadline: "2025.08.01 ~ 2025.09.01",
+      ),
+      PromotionData(
+        imageURL: "assets/images/test_Mask group.jpg",
+        title: "맥도날드 맥런치",
+        deadline: "상시",
+      ),
+      PromotionData(
+        imageURL: "assets/images/test_Mask group.jpg",
+        title: "맘스터치 신메뉴",
+        deadline: "2025.11.31 ~ 2025.12.31",
+      ),
+    ],
+  };
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _selectedCategory = widget.initialCategory;
   }
@@ -68,6 +99,8 @@ class _PromoScreenState extends State<PromoScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final currentPromotions = _dummyData[_selectedCategory] ?? [];
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -79,7 +112,7 @@ class _PromoScreenState extends State<PromoScreen> {
                 height: 90,
                 child: ListView(
                   scrollDirection: Axis.horizontal,
-                  children: MenuCategory.values.map((category){
+                  children: MenuCategory.values.map((category) {
                     return _buildMenuItem(category);
                   }).toList(),
                 ),
@@ -136,56 +169,36 @@ class _PromoScreenState extends State<PromoScreen> {
                 ),
               ),
               SizedBox(height: 25),
+              //promo view
               Expanded(
-                child: ListView(
-                  //promo view
-                  children: [
-                    PromotionBlock(
-                      imageURL: "assets/images/test_Mask group.jpg",
-                      title: "놓치지마 32% 할인",
-                      deadline: "2025.09.12 ~ 2026.05.09",
-                      onPressed: (){
-                        Navigator.push(
-                          context,
-                            MaterialPageRoute(
-                                builder: (context) => DetailPromotion())
-                        );
-                      },
-                    ),
-                    PromotionBlock(
-                      imageURL: "assets/images/test_Mask group.jpg",
-                      title: "공차데이",
-                      deadline: "2025.10.11",
-                      onPressed: (){
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => DetailPromotion())
-                        );
-                      },
-                    ),
-                    PromotionBlock(
-                      imageURL: "assets/images/test_Mask group.jpg",
-                      title: "공차데이2",
-                      deadline: "2025.10.11",
-                      onPressed: (){
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => DetailPromotion())
-                        );
-                      },
-                    ),
-                  ],
-                  // click promo -> detail page로 넘어가도록
-                ),
+                child: currentPromotions.isEmpty
+                    ? Center(child: Text("등록된 프로모션이 없습니다."))
+                    : ListView.builder(
+                        itemCount: currentPromotions.length,
+                        itemBuilder: (context, index) {
+                          final data = currentPromotions[index];
+                          return PromotionBlock(
+                            imageURL: data.imageURL,
+                            title: data.title,
+                            deadline: data.deadline,
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => DetailPromotion(),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
               ),
             ],
           ),
         ),
       ),
       floatingActionButton: FloatingSearchButton(
-        onPressed: (){
+        onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => SearchPromotion()),
