bb06a8659379468495d9823224c265e0844462bc
Yujin <0505ujin@naver.com>
2025-11-27 15:46:47 +0900

    design: search bar padding 조절

--- frontend/lib/screens/brand-promotion/search_promo_screen.dart
+++ frontend/lib/screens/brand-promotion/search_promo_screen.dart
@@ -25,32 +25,35 @@ class _SearchPromotionState extends State<SearchPromotion> {
         backgroundColor: Colors.white,
         elevation: 0,
         iconTheme: IconThemeData(color: Color(0xFFAFB8C1)),
-        title: SearchBar(
-          controller: _textController,
-          hintText: "매장명, 프로모션 검색",
-          hintStyle: WidgetStateProperty.all(
-            TextStyle(
-              color: Colors.grey[400],
-              fontFamily: "Pretendard",
-              fontSize: 18,
-              fontWeight: FontWeight.w500,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: SearchBar(
+            controller: _textController,
+            hintText: "매장명, 프로모션 검색",
+            hintStyle: WidgetStateProperty.all(
+              TextStyle(
+                color: Colors.grey[400],
+                fontFamily: "Pretendard",
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
             ),
+            backgroundColor: WidgetStateProperty.all<Color>(Colors.grey[050]!),
+            elevation: WidgetStateProperty.all<double>(0.0),
+            trailing: [
+              IconButton(onPressed: () {_textController.clear();}, icon: Icon(Icons.cancel)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            ],
+            shape: WidgetStateProperty.all(
+              ContinuousRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            ),
+            constraints: BoxConstraints(maxHeight: 60),
           ),
-          backgroundColor: WidgetStateProperty.all<Color>(Colors.grey[050]!),
-          elevation: WidgetStateProperty.all<double>(0.0),
-          trailing: [
-            IconButton(onPressed: () {_textController.clear();}, icon: Icon(Icons.cancel)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-          ],
-          shape: WidgetStateProperty.all(
-            ContinuousRectangleBorder(borderRadius: BorderRadius.circular(12)),
-          ),
-          constraints: BoxConstraints(maxHeight: 60),
         ),
       ),
       // 인기 검색어
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.fromLTRB(30.0,20.0,20.0,25.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
